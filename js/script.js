f9ca788620d55d57415f2ee69110146d28618b46
Joseph Nyaga <joseph.kinyua@student.moringaschool.com>
2021-06-21 20:29:44 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $(".image1").click(function() {
+        $("#pic1").slideToggle();
+        $("#boom1").slideToggle();
+    });
+    $(".image2").click(function() {
+        $("#pic2").slideToggle();
+        $("#boom2").slideToggle();
+    });
+    $(".image3").click(function() {
+        $("#pic3").slideToggle();
+        $("#boom3").slideToggle();
+    });
+
+});
+$(document).ready(function() {
+    $("#wek1").hover(function() {
+        $("#port1").toggle();
+    });
+    $("#wek2").hover(function() {
+        $("#port2").toggle();
+    });
+    $("#wek3").hover(function() {
+        $("#port3").toggle();
+    });
+    $("#wek4").hover(function() {
+        $("#port4").toggle();
+    });
+    $("#wek5").hover(function() {
+        $("#port5").toggle();
+    });
+    $("#wek6").hover(function() {
+        $("#port6").toggle();
+    });
+    $("#wek7").hover(function() {
+        $("#port7").toggle();
+    });
+    $("#wek8").hover(function() {
+        $("#port8").toggle();
+    });
+});
